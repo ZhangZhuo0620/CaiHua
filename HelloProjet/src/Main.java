7a59e21eaba21fdf9c79d2cc2fba69ad8c80c89e
CaiHua <caihua88@163.com>
2020-03-20 22:37:59 +0800

    彩画第一次新增

--- HelloProjet/src/Main.java
+++ HelloProjet/src/Main.java
@@ -5,7 +5,14 @@ public class Main {
     @Test
     public void testHelloWrold() {
         System.out.println("HelloWorld!");
-		System.out.println("新增第一次！！！");
+		System.out.println("AA新增第一次！！！");
+		System.out.println("彩画新增第一次！！！");
+		
+		
+		
+		
+		
+
 
     }
 }
